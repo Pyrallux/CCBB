7ae9bf2ef3488c292d421e0f1b63a4b81ab0dcd0
Nate Nevois <nnnevwa630@gmail.com>
2024-01-10 22:26:43 -0600

    Backend Rework; Basic API Functionality Implemented

--- Backend/cyclecount/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CyclecountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cyclecount'
--- Backend/cyclecount/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# request -> response
-def say_hello(request):
-    return HttpResponse('Balls')
