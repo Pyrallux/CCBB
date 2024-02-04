f0eceb32ec9e391d7da9fc6feda7fb24b8469299
Nate Nevois <nnnevwa630@gmail.com>
2024-02-03 22:41:44 -0600

    Added system and present parts API's (needed for future feature addition)

---
+++ Frontend/ccbb-frontend/src/api/systemPartsApi.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+interface SystemPart {
+    system_part_id?: number,
+    number: string,
+    quantity: number,
+    bin_id: number,
+}
+
+const systemPartApi = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+export const getSystemParts = async () => {
+  const response = await systemPartApi.get("/system_parts/");
+  return response.data;
+};
+
+export const getSystemPartDetail = async (system_part_id: number) => {
+  const response = await systemPartApi.get(`/system_parts/${system_part_id}`);
+  return response.data;
+};
+
+export const getSystemPartParent = async (parent_id: number) => {
+  const response = await systemPartApi.get(`/system_parts/parent/${parent_id}`);
+  return response.data;
+};
+
+export const addSystemPart = async (system_part: SystemPart) => {
+  return await systemPartApi.post("/system_parts/", system_part);
+};
+
+export const updateSystemPart = async (system_part: SystemPart) => {
+  return await systemPartApi.put(
+    `/system_parts/${system_part.system_part_id}`,
+    system_part
+  );
+};
+
+export const deleteSystemPart = async (system_part_id: number) => {
+  return await systemPartApi.delete(`/system_parts/${system_part_id}`);
+};
+
+export default systemPartApi;
